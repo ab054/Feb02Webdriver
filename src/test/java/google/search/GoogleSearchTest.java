1ba51184f3f1f07f6c67d7cd6270a44e187a1a2e
ab054 <alfamiliya@gmail.com>
2021-05-26 21:18:49 -0700

    day09: code

--- src/test/java/google/search/GoogleSearchTest.java
+++ src/test/java/google/search/GoogleSearchTest.java
@@ -3,13 +3,10 @@ package google.search;
 import google.search.pages.MainPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import uiTests.BaseTest;
-
-
 
 public class GoogleSearchTest extends BaseTest {
 
@@ -32,12 +29,47 @@ public class GoogleSearchTest extends BaseTest {
         verifyResultsPage();
     }
 
-    private void waitForElement() {
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("result-stats")));
+    public void waitForElement(By element){
 
-        //wait.until(webDriver -> webDriver.findElement(By.id("result-stats")).isDisplayed());
     }
+
+    //1. open browser
+    //2. go to Google main page
+    //3. type search query
+    //4. submit query
+    //5. verify Results Page is opened
+    @Parameters({ "queryValue" })
+    @Test(groups = "brokenTests")
+    public void test0002(String testData1) {
+        MainPage mainPage = new MainPage(driver);
+        //ResultsPage resultsPage = new ResultPage(driver);
+
+        mainPage.open();
+        mainPage.typeSearchQuery(testData1);
+        mainPage.submitQuery();
+
+        //waitFor(5);
+        waitForElement();
+        verifyResultsPage();
+    }
+
+
+    @Test(dataProvider = "dataProvider001")
+    public void test0003(String testData1) {
+        MainPage mainPage = new MainPage(driver);
+        //ResultsPage resultsPage = new ResultPage(driver);
+
+        mainPage.open();
+        mainPage.typeSearchQuery(testData1);
+        mainPage.submitQuery();
+
+        //waitFor(5);
+        waitForElement();
+        verifyResultsPage();
+    }
+
+
+
 
     private void verifyResultsPage() {
         WebElement element = driver.findElement(By.id("result-stats"));
--- src/test/java/uiTests/BaseTest.java
+++ src/test/java/uiTests/BaseTest.java
@@ -1,8 +1,12 @@
 package uiTests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
 
 import static java.lang.Thread.sleep;
 
@@ -22,5 +26,20 @@ public class BaseTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @DataProvider(name = "dataProvider001")
+    public Object[][] createData1() {
+        return new Object[][] {
+                { "Portnov Computer School" },
+                { "Portnov Computer" },
+                { "Amazon "},
+        };
+    }
+
+    public void waitForElement() {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("result-stats")));
+        //wait.until();
     }
 }
